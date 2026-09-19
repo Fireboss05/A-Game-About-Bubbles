2c193e08b8e9713d0f9a403b7db1ddb72b5be3e4
Sacha <sachaterrasson@gmail.com>
2026-09-19 13:49:47 +0200

    main: Created a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::{DefaultPlugins, app::prelude::*};
+
+
 fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_systems(Startup, setup);
+    app.add_systems(Update, update);
+
+    app.run();
+}
+
+fn setup() {
     println!("Hello, world!");
+}
+
+fn update() {
+    println!("Update");
 }
